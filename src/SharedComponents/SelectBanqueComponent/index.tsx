cc1ec8b5e468edb6d0602b7da9e4ae4c1f319439
eyaboulaabi <eyaboulaabi1@gmail.com>
2024-02-14 12:30:26 +0100

    commit1

---
+++ src/SharedComponents/SelectBanqueComponent/index.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import Grid from '@mui/material/Grid'
+import FormControl from '@mui/material/FormControl'
+import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker';
+import Select from 'react-select';
+import { getOptions } from './mockBanque';
+//@ts-ignore
+import  styles from '../styles.module.scss'
+
+
+interface Option {
+  value: string;
+  label: string;
+}
+const SelectBanque = ({ onSearch, labelText }: any) => {
+
+  const [options, setOptions] = useState<Option[]>([]);
+  const [statusValue, setStatusValue] = useState('');
+  const [isClearable, setIsClearable] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getOptions();
+        setOptions(response.options);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des options :', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  return (
+    <DatePickerWrapper>
+      <Grid container spacing={6}>
+        <Grid item xs={12}>
+          <Grid container spacing={6}>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth sx={{ mr: 4, mb: 2 }}>
+
+                <Select placeholder={'---Sélectionnez une banque---'}
+                  defaultValue={statusValue}
+                  onChange={(value: any) => {
+                   onSearch ? onSearch():  setStatusValue(value)
+                   labelText 
+                  }}
+                  options={options} 
+                  isClearable={isClearable}
+                  className={styles.Select1}
+                  />
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </DatePickerWrapper>
+
+  )
+}
+
+export default SelectBanque;
