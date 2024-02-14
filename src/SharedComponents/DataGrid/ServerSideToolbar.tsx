82d3ebe0ac6c4956465acfaa597270d19c493412
Dakhloaui moataz <moatez@shamash-it.com>
2024-02-14 14:27:06 +0100

    exemple of callback function added the function of the number of rows per page

--- src/SharedComponents/DataGrid/ServerSideToolbar.tsx
+++ src/SharedComponents/DataGrid/ServerSideToolbar.tsx
@@ -13,78 +13,81 @@ import FormControl from '@mui/material/FormControl'
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
 
-interface Props {
+interface ServerSideToolbarProps {
   value: string
   clearSearch: () => void
   onChange: (e: ChangeEvent) => void
+  onNumberRowPageChange: (value: string) => void
 }
 
-const ServerSideToolbar = (props: Props) => {
+const ServerSideToolbar = (props: ServerSideToolbarProps) => {
+  const { clearSearch, onChange, onNumberRowPageChange, value } = props
   return (
     <>
+      <FormControl>
+        <InputLabel htmlFor='outlined-age-native-simple'>Rows per page</InputLabel>
+        <Select
+          native
+          label='Rows per page'
+          onChange={event => {
+            onNumberRowPageChange && onNumberRowPageChange(event.target.value)
+          }}
+          defaultValue=''
+          sx={{
+            height: '5vh',
+            alignContent: 'center'
+          }}
+          inputProps={{
+            name: 'Rows per page',
+            id: 'outlined-age-native-simple'
+          }}
+        >
+          <option aria-label='None' value='' />
+          <option value={10}>Ten</option>
+          <option value={20}>Twenty</option>
+          <option value={30}>Thirty</option>
+        </Select>
+      </FormControl>
 
-    <FormControl>
-    <InputLabel htmlFor='outlined-age-native-simple'>Rows per page</InputLabel>
-    <Select
-      native
-      label='Rows per page'
-      defaultValue=''
-      sx={{
-        height: '5vh',
-        alignContent: 'center'
-      }}
-      inputProps={{
-        name: 'Rows per page',
-        id: 'outlined-age-native-simple'
-      }}
-    >
-      <option aria-label='None' value='' />
-      <option value={10}>Ten</option>
-      <option value={20}>Twenty</option>
-      <option value={30}>Thirty</option>
-    </Select>
-  </FormControl>
-
-
-    <Box
-      sx={{
-        gap: 2,
-        display: 'flex',
-        flexWrap: 'wrap',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        p: theme => theme.spacing(2, 5, 4, 5)
-      }}
-    >
-      <GridToolbarExport printOptions={{ disableToolbarButton: true }} />
-      <TextField
-        size='small'
-        value={props.value}
-        onChange={props.onChange}
-        placeholder='Search…'
-        InputProps={{
-          startAdornment: (
-            <Box sx={{ mr: 2, display: 'flex' }}>
-              <Icon icon='tabler:search' fontSize={20} />
-            </Box>
-          ),
-          endAdornment: (
-            <IconButton size='small' title='Clear' aria-label='Clear' onClick={props.clearSearch}>
-              <Icon icon='tabler:x' fontSize={20} />
-            </IconButton>
-          )
-        }}
+      <Box
         sx={{
-          width: {
-            xs: 1,
-            sm: 'auto'
-          },
-          '& .MuiInputBase-root > svg': {
-            mr: 2
-          }
+          gap: 2,
+          display: 'flex',
+          flexWrap: 'wrap',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          p: theme => theme.spacing(2, 5, 4, 5)
         }}
-      />
-    </Box>
+      >
+        <GridToolbarExport printOptions={{ disableToolbarButton: true }} />
+        <TextField
+          size='small'
+          value={value}
+          onChange={onChange}
+          placeholder='Search…'
+          InputProps={{
+            startAdornment: (
+              <Box sx={{ mr: 2, display: 'flex' }}>
+                <Icon icon='tabler:search' fontSize={20} />
+              </Box>
+            ),
+            endAdornment: (
+              <IconButton size='small' title='Clear' aria-label='Clear' onClick={clearSearch}>
+                <Icon icon='tabler:x' fontSize={20} />
+              </IconButton>
+            )
+          }}
+          sx={{
+            width: {
+              xs: 1,
+              sm: 'auto'
+            },
+            '& .MuiInputBase-root > svg': {
+              mr: 2
+            }
+          }}
+        />
+      </Box>
     </>
   )
 }
