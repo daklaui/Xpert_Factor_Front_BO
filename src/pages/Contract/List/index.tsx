ffedede9aaa480c97406febc8ac53112bdd38457
AmineKhadhraoui <aminekhadhraoui2017@gmail.com>
2024-02-17 19:19:57 +0100

    Contract List Screen pushed for review

--- src/pages/Contract/List/index.tsx
+++ src/pages/Contract/List/index.tsx
@@ -7,47 +7,65 @@ import { columns } from './components/columns'
 import generateFakeData from '../mock/Data.mock'
 
 const IndividualList = ({ fakeData }: any) => {
+const IndividualList = ({ fakeData }: any) => {
   const defaultPageSize = 10
   const [pageSize, setPageSize] = useState<number>(defaultPageSize)
   const [page, setPage] = useState<number>(1)
-  const [totalPages, setTotalPages] = useState<number>(0)
-  const [contract, setContract] = useState<DataGridRowType[]>([])
+  const [pages, setTotalPages] = useState<number>(0)
+  const [individus, setIndividus] = useState<DataGridRowType[]>([])
   const [filteredData, setFilteredData] = useState<DataGridRowType[]>([])
 
   useEffect(() => {
-    const filteredContract = fakeData.map((row: DataGridRowType, index: number) => ({
-      id: index + 1,
-      Ref_contrat: row.Ref_contrat
-      // Add other properties here
+    const filteredData = fakeData.map((row: DataGridRowTypeContract, index: number) => ({
+      id: index + 1, // Generate a unique identifier
+      Ref_contrat: row.Ref_contrat,
+      Nom_Adherenet: row.Nom_Adherenet,
+      Encours_des_factures: row.Encours_des_factures,
+      Total_Factures: row.Total_Factures,
+      Total_Financements: row.Total_Financements,
+      Total_Encaissement: row.Total_Encaissement,
+      Total_Avoirs: row.Total_Avoirs,
+      Total_Intérets: row.Total_Intérets,
+      Limite_De_Financement: row.Limite_De_Financement,
+      Devise: row.Devise,
+      Nombre_Dacheteurs_prévues: row.Nombre_Dacheteurs_prévues,
+      Nombre_De_Factures_prévues: row.Nombre_De_Factures_prévues,
+      Nombre_Davoirs_prévues: row.Nombre_Davoirs_prévues,
+      Nombre_De_remise_prévues: row.Nombre_De_remise_prévues,
+      Délai_moyen_de_réglement: row.Délai_moyen_de_réglement,
+      Délai_max_de_réglement: row.Délai_max_de_réglement
     }))
-    setContract(filteredContract)
+
+    setIndividus(filteredData)
   }, [fakeData])
 
   useEffect(() => {
-    if (contract.length > 0) {
-      const { currentPageItems, totalPages } = paginate<DataGridRowType>(contract, { currentPage: 1, pageSize })
+    if (individus.length > 0) {
+      const { currentPageItems, totalPages } = paginate<DataGridRowType>(individus, { currentPage: 1, pageSize })
       setTotalPages(totalPages)
       setFilteredData(currentPageItems)
     }
-  }, [contract, pageSize])
+  }, [individus, pageSize])
 
   const onSearch = (text: string) => {
     const lowercaseQuery = text.toLowerCase()
-    const searchData = contract.filter((item: DataGridRowType) =>
-      Object.values(item).some(value => typeof value === 'string' && value.toLowerCase().includes(lowercaseQuery))
-    )
+    const searchData = individus.filter((item: DataGridRowType) => {
+      return Object.values(item).some(
+        value => typeof value === 'string' && value.toLowerCase().includes(lowercaseQuery)
+      )
+    })
     setFilteredData(searchData)
   }
 
   const onPageChange = (index: number) => {
-    const { currentPageItems, totalPages } = paginate<DataGridRowType>(contract, { currentPage: index, pageSize })
+    const { currentPageItems, totalPages } = paginate<DataGridRowType>(individus, { currentPage: index, pageSize })
     setPage(index)
     setTotalPages(totalPages)
     setFilteredData(currentPageItems)
   }
 
   const onNumberRowPageChange = (numberOfRows: string) => {
-    const { currentPageItems, totalPages } = paginate<DataGridRowType>(contract, {
+    const { currentPageItems, totalPages } = paginate<DataGridRowType>(individus, {
       currentPage: page,
       pageSize: parseInt(numberOfRows)
     })
@@ -57,7 +75,7 @@ const IndividualList = ({ fakeData }: any) => {
   }
 
   const onSort = (value: DataGridSortObject) => {
-    const sortedData = customSort(contract, { key: value.field, order: value.sort })
+    const sortedData = customSort(individus, { key: value.field, order: value.sort })
     const { currentPageItems } = paginate<DataGridRowType>(sortedData, { currentPage: page, pageSize })
     setFilteredData(currentPageItems)
   }
@@ -80,11 +98,12 @@ const IndividualList = ({ fakeData }: any) => {
       onPageChange={onPageChange}
       columns={columns}
       onCustomSort={onSort}
-      title={'Liste des contrats'}
+      title={'Individu List'}
     />
   )
 }
 
+export default IndividualList
 export default IndividualList
 
 export async function getStaticProps() {
