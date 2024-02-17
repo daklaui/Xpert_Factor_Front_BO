d37a45a17dd502044a830f120ae7725753abb30a
AmineKhadhraoui <aminekhadhraoui2017@gmail.com>
2024-02-17 19:19:57 +0100

    Contract List Screen pushed for review

--- src/pages/Contract/List/index.tsx
+++ src/pages/Contract/List/index.tsx
@@ -6,17 +6,29 @@ import { customSort } from 'src/@core/utils/customSort'
 import { columns } from './components/columns'
 import generateFakeData from '../mock/Data.mock'
 
+<<<<<<< HEAD
 const ContractList = ({ fakeData }: any) => {
+=======
+const IndividualList = ({ fakeData }: any) => {
+>>>>>>> 4ee6d72 (Contract List Screen pushed for review)
   const defaultPageSize = 10
   const [pageSize, setPageSize] = useState<number>(defaultPageSize)
   const [page, setPage] = useState<number>(1)
   const [pages, setTotalPages] = useState<number>(0)
+<<<<<<< HEAD
   const [contract, setContract] = useState<DataGridRowType[]>([])
+=======
+  const [individus, setIndividus] = useState<DataGridRowType[]>([])
+>>>>>>> 4ee6d72 (Contract List Screen pushed for review)
   const [filteredData, setFilteredData] = useState<DataGridRowType[]>([])
 
   useEffect(() => {
     const filteredData = fakeData.map((row: DataGridRowTypeContract, index: number) => ({
+<<<<<<< HEAD
       id: index + 1,
+=======
+      id: index + 1, // Generate a unique identifier
+>>>>>>> 4ee6d72 (Contract List Screen pushed for review)
       Ref_contrat: row.Ref_contrat,
       Nom_Adherenet: row.Nom_Adherenet,
       Encours_des_factures: row.Encours_des_factures,
@@ -35,6 +47,7 @@ const ContractList = ({ fakeData }: any) => {
       Délai_max_de_réglement: row.Délai_max_de_réglement
     }))
 
+<<<<<<< HEAD
     setContract(filteredData)
   }, [fakeData])
 
@@ -49,6 +62,22 @@ const ContractList = ({ fakeData }: any) => {
   const onSearch = (text: string) => {
     const lowercaseQuery = text.toLowerCase()
     const searchData = contract.filter((item: DataGridRowType) => {
+=======
+    setIndividus(filteredData)
+  }, [fakeData])
+
+  useEffect(() => {
+    if (individus.length > 0) {
+      const { currentPageItems, totalPages } = paginate<DataGridRowType>(individus, { currentPage: 1, pageSize })
+      setTotalPages(totalPages)
+      setFilteredData(currentPageItems)
+    }
+  }, [individus, pageSize])
+
+  const onSearch = (text: string) => {
+    const lowercaseQuery = text.toLowerCase()
+    const searchData = individus.filter((item: DataGridRowType) => {
+>>>>>>> 4ee6d72 (Contract List Screen pushed for review)
       return Object.values(item).some(
         value => typeof value === 'string' && value.toLowerCase().includes(lowercaseQuery)
       )
@@ -57,14 +86,22 @@ const ContractList = ({ fakeData }: any) => {
   }
 
   const onPageChange = (index: number) => {
+<<<<<<< HEAD
     const { currentPageItems, totalPages } = paginate<DataGridRowType>(contract, { currentPage: index, pageSize })
+=======
+    const { currentPageItems, totalPages } = paginate<DataGridRowType>(individus, { currentPage: index, pageSize })
+>>>>>>> 4ee6d72 (Contract List Screen pushed for review)
     setPage(index)
     setTotalPages(totalPages)
     setFilteredData(currentPageItems)
   }
 
   const onNumberRowPageChange = (numberOfRows: string) => {
+<<<<<<< HEAD
     const { currentPageItems, totalPages } = paginate<DataGridRowType>(contract, {
+=======
+    const { currentPageItems, totalPages } = paginate<DataGridRowType>(individus, {
+>>>>>>> 4ee6d72 (Contract List Screen pushed for review)
       currentPage: page,
       pageSize: parseInt(numberOfRows)
     })
@@ -74,7 +111,11 @@ const ContractList = ({ fakeData }: any) => {
   }
 
   const onSort = (value: DataGridSortObject) => {
+<<<<<<< HEAD
     const sortedData = customSort(contract, { key: value.field, order: value.sort })
+=======
+    const sortedData = customSort(individus, { key: value.field, order: value.sort })
+>>>>>>> 4ee6d72 (Contract List Screen pushed for review)
     const { currentPageItems } = paginate<DataGridRowType>(sortedData, { currentPage: page, pageSize })
     setFilteredData(currentPageItems)
   }
@@ -97,12 +138,20 @@ const ContractList = ({ fakeData }: any) => {
       onPageChange={onPageChange}
       columns={columns}
       onCustomSort={onSort}
+<<<<<<< HEAD
       title={'Liste des contrats'}
+=======
+      title={'Individu List'}
+>>>>>>> 4ee6d72 (Contract List Screen pushed for review)
     />
   )
 }
 
+<<<<<<< HEAD
 export default ContractList
+=======
+export default IndividualList
+>>>>>>> 4ee6d72 (Contract List Screen pushed for review)
 
 export async function getStaticProps() {
   const fakeData = generateFakeData(60)
