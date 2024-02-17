a801a8f57e487dc5e2359e55f807449e4453b8b1
Anis Allagui <annousanisallag123@gmail.com>
2024-02-17 13:51:20 +0100

    AddContractForm + Styled TextField (shared Component) Anis_First_Push

--- src/shared-components/custom-select/SelectListOfDevises/index.tsx
+++ src/shared-components/custom-select/SelectListOfDevises/index.tsx
@@ -4,10 +4,14 @@ import Select from 'react-select'
 import { CustomSelectProps, SelectInerface } from '../interface/customSelect.interface'
 import { getDeviseList } from '../mock'
 
+<<<<<<< HEAD:src/shared-components/custom-select/SelectListOfDevises/index.tsx
 const SelectListOfDevises = ({ onSearch }: CustomSelectProps) => {
+=======
+const SelectDevise = ({ onSearch, }: CustomSelectProps) => {
+>>>>>>> 9d89388 (AddContractForm + Styled TextField (shared Component) Anis_First_Push):src/SharedComponents/CustomSelect/SelectDeviseComponent/index.tsx
   const [options, setOptions] = useState<SelectInerface[]>([])
   const [statusValue, setStatusValue] = useState('')
-  const [isClearable, setIsClearable] = useState(true)
+  const [isClearable, ] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
