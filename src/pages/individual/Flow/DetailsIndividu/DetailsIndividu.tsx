53e3187cc2a030d75c253f4ab64cd1a0f04eab87
eyaboulaabi <eyaboulaabi1@gmail.com>
2024-02-17 17:27:51 +0100

    interface details individual

--- src/pages/individual/Flow/DetailsIndividu/DetailsIndividu.tsx
+++ src/pages/individual/Flow/DetailsIndividu/DetailsIndividu.tsx
@@ -1,88 +1,108 @@
-import { forwardRef, useState } from 'react'
+import { useState } from 'react'
 import Tab from '@mui/material/Tab'
 import Card from '@mui/material/Card'
 import TabList from '@mui/lab/TabList'
-import TabPanel from '@mui/lab/TabPanel'
-import Button from '@mui/material/Button'
-import Divider from '@mui/material/Divider'
 import TabContext from '@mui/lab/TabContext'
-import CardContent from '@mui/material/CardContent'
-import CardActions from '@mui/material/CardActions'
 import PersonIcon from '@mui/icons-material/PersonAddAlt'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
-import LoginIcon from '@mui/icons-material/Login';
-import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
-import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import LoginIcon from '@mui/icons-material/Login'
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered'
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone'
 import InfoIndividu from './InfoIndividu'
+import AutreInfo from './AutreInfo'
+import AutreContact from './AutreContact'
+import LoginWeb from './LoginWeb'
+import ListeRib from './ListeRib'
+import { Button, CardHeader, Typography, Grid } from '@mui/material'
+
 const DetailsIndividu = () => {
   // ** States
-  const [value, setValue] = useState('personal-info')
+  const [value, setValue] = useState('info-individu')
 
   const handleTabsChange = (event: React.ChangeEvent<any>, newValue: string) => {
     setValue(newValue)
   }
 
   return (
-    <Card>
-      <TabContext value={value}>
-        <TabList
-          variant='scrollable'
-          scrollButtons={false}
-          onChange={handleTabsChange}
-          sx={{
-            borderBottom: theme => `1px solid ${theme.palette.divider}`,
-            '& .MuiTab-root': { py: 1.5, width: '210px' }
-          }}
-        >
-          <Tab
-            value='info-individu'
-            label={
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <AccountCircleIcon style={{ marginRight: 8 }} />
-                Info d'individu
-              </div>
-            }
-          />
-          <Tab
-            value='autre-informations'
-            label={
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <PersonIcon style={{ marginRight: 8 }} />
-                Autre informations
-              </div>
-            }
-          />
-          <Tab
-            value='autre-contacts'
-            label={
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <ContactPhoneIcon style={{ marginRight: 8 }} />
-                Autre contacts
-              </div>
-            }
-          />
-           <Tab
-            value='login-web'
-            label={
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <LoginIcon style={{ marginRight: 8 }} />
-                Login web
-              </div>
-            }
-          />
-           <Tab
-            value='liste-RIB'
-            label={
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <FormatListNumberedIcon style={{ marginRight: 8 }} />
-                Liste RIB
-              </div>
-            }
-          />
-        </TabList>
-       <InfoIndividu/> 
-      </TabContext>
-    </Card>
+    <>
+      <Grid container alignItems='center' justifyContent='space-between'>
+        <Typography variant='h2' component='div' sx={{ mb: 2 }}>
+          Individu
+        </Typography>
+        <Grid container item justifyContent='flex-end' spacing={1}>
+          <Button variant='contained'>Modifier Individu</Button>
+          <Button variant='contained' sx={{ marginLeft: '8px' }}>
+            Liste des Individus
+          </Button>
+        </Grid>
+      </Grid>
+      <Card>
+        <CardHeader title={<span style={{ fontSize: '20px' }}>Details d'individu</span>} />
+
+        <TabContext value={value}>
+          <TabList
+            variant='scrollable'
+            scrollButtons={false}
+            onChange={handleTabsChange}
+            sx={{
+              borderBottom: theme => `1px solid ${theme.palette.divider}`,
+              '& .MuiTab-root': { py: 1.5, width: '210px' }
+            }}
+          >
+            <Tab
+              value='info-individu'
+              label={
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <AccountCircleIcon style={{ marginRight: 8 }} />
+                  Info d'individu
+                </div>
+              }
+            />
+            <Tab
+              value='autre-informations'
+              label={
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <PersonIcon style={{ marginRight: 8 }} />
+                  Autre informations
+                </div>
+              }
+            />
+            <Tab
+              value='autre-contacts'
+              label={
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <ContactPhoneIcon style={{ marginRight: 8 }} />
+                  Autre contacts
+                </div>
+              }
+            />
+            <Tab
+              value='login-web'
+              label={
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <LoginIcon style={{ marginRight: 8 }} />
+                  Login web
+                </div>
+              }
+            />
+            <Tab
+              value='liste-RIB'
+              label={
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <FormatListNumberedIcon style={{ marginRight: 8 }} />
+                  Liste RIB
+                </div>
+              }
+            />
+          </TabList>
+          {value === 'info-individu' && <InfoIndividu />}
+          {value === 'autre-informations' && <AutreInfo />}
+          {value === 'autre-contacts' && <AutreContact />}
+          {value === 'login-web' && <LoginWeb />}
+          {value === 'liste-RIB' && <ListeRib />}
+        </TabContext>
+      </Card>
+    </>
   )
 }
 export default DetailsIndividu
--- src/pages/individual/List/index.tsx
+++ src/pages/individual/List/index.tsx
@@ -1,46 +1,11 @@
-import SelectAdherent from "src/SharedComponents/CustomSelect/SelectAdherentComponent";
-import SelectBanque from "src/SharedComponents/CustomSelect/SelectBanqueComponent";
-import SelectDevise from "src/SharedComponents/CustomSelect/SelectDeviseComponent";
-import SelectIndividu from "src/SharedComponents/CustomSelect/SelectIndividualComponent";
-import SelectModePaiement from "src/SharedComponents/CustomSelect/SelectModePaimentComponent";
+import DetailsIndividu from '../Flow/DetailsIndividu/DetailsIndividu'
 
 const IndividualList = () => {
-  return <>
-
-    <SelectIndividu onSearch={
-      (value: any) => {
-        console.log(value);
-      }
-    } labelText='Individu' />
-
-   < SelectAdherent onSearch={
-      (value: any) => {
-        console.log(value);
-      }
-    } labelText='Adherent' />
-
-
-    <SelectDevise onSearch={
-      (value: any) => {
-        console.log(value);
-      }
-    } labelText='Devise'/>
-
-
-    <SelectBanque  onSearch={
-      (value: any) => {
-        console.log(value);
-      }
-    } labelText='Banque'/>
-
-
-    <SelectModePaiement  onSearch={
-      (value: any) => {
-        console.log(value);
-      }
-    } labelText='Paiement'/>
-
-  </>
+  return (
+    <>
+      <DetailsIndividu />
+    </>
+  )
 }
 
 export default IndividualList
