e7ae4d3cc2323ce497a3c9ed0620b00f00efaf3c
Dakhloaui moataz <moatez@shamash-it.com>
2024-02-17 18:11:26 +0100

    cleaned the datagrid code fixed some issues

--- src/shared-components/custom-select/SelectListOfDevises/index.tsx
+++ src/shared-components/custom-select/SelectListOfDevises/index.tsx
@@ -4,19 +4,14 @@ import Select from 'react-select'
 import { CustomSelectProps, SelectInerface } from '../interface/customSelect.interface'
 import { getDeviseList } from '../mock'
 
-<<<<<<< HEAD:src/shared-components/custom-select/SelectListOfDevises/index.tsx
 const SelectListOfDevises = ({ onSearch }: CustomSelectProps) => {
-=======
-const SelectDevise = ({ onSearch, }: CustomSelectProps) => {
->>>>>>> 9d89388 (AddContractForm + Styled TextField (shared Component) Anis_First_Push):src/SharedComponents/CustomSelect/SelectDeviseComponent/index.tsx
   const [options, setOptions] = useState<SelectInerface[]>([])
   const [statusValue, setStatusValue] = useState('')
-  const [isClearable, ] = useState(true)
+  const [isClearable] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await getDeviseList()
         const response = await getDeviseList()
         setOptions(response.options)
       } catch (error) {
@@ -40,5 +35,4 @@ const SelectDevise = ({ onSearch, }: CustomSelectProps) => {
   )
 }
 
-export default SelectListOfDevises
 export default SelectListOfDevises
