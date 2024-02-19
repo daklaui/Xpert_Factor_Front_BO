23ca27561c6bd12692b1a03a0c9e60c350acd4d8
eyaboulaabi <eyaboulaabi1@gmail.com>
2024-02-20 00:13:18 +0100

    details individu with lazy loading

---
+++ src/pages/individual/[id]/LoginWeb.tsx
@@ -0,0 +1,35 @@
+import TabPanel from '@mui/lab/TabPanel'
+import {
+  CardContent,
+  Checkbox,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from '@mui/material'
+import { Suspense, lazy } from 'react'
+import TableLogin from './tables/TableLogin'
+
+const TabLogin = lazy(() => import('./tables/TableLogin'))
+
+const LoginWeb = () => {
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <CardContent>
+        <TabPanel sx={{ p: 0 }} value='login-web'>
+          <Grid container spacing={5}>
+            <Grid item xs={12}>
+              <Suspense fallback={<div>Loading...</div>}>
+                <TableLogin />
+              </Suspense>
+            </Grid>
+          </Grid>
+        </TabPanel>
+      </CardContent>
+    </form>
+  )
+}
+export default LoginWeb
