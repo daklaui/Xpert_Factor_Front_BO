7abf5d7db223ef723a088f2f0020b8eab7a28722
Anis Allagui <annousanisallag123@gmail.com>
2024-02-19 12:07:04 +0100

    styled text field fake modification

--- src/pages/Contract/Flow/index.tsx
+++ src/pages/Contract/Flow/index.tsx
@@ -1,1 +1,13 @@
 
+import React from 'react'
+import GeneralInfo from './GeneralInfo'
+
+function index() {
+  const popperPlacement = 'bottom';
+  
+  return (
+      <GeneralInfo popperPlacement={popperPlacement} />
+  )
+}
+
+export default index
