27e864673092bf1f42bda9d02272ee7d38837e95
Dakhloaui moataz <moatez@shamash-it.com>
2024-02-19 21:14:58 +0100

    changes

--- src/shared-components/StyledTextField/StyledTextField .tsx
+++ src/shared-components/StyledTextField/StyledTextField .tsx
@@ -1,30 +1,51 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+import { Controller } from 'react-hook-form';
 
-import React from 'react'
-import TextField from '@mui/material/TextField'
-import { styled } from '@mui/material/styles'
-import CustomTextFieldProps from 'src/shared-components/StyledTextField/Interfaces/CustomTextFiledInterface'
-import StyledLabel from 'src/pages/Contract/Flow/StyledInputs/StyledLabel'
-const StyledTextField : any = styled(TextField)(({ fullWidth }: CustomTextFieldProps) => ({
+const StyledLabel = styled('label')({
+  fontWeight: 'bold',
+  fontSize: '13px',
+  paddingBottom: '12px',
+  marginBottom: '110px',
+});
+
+const StyledTextField = styled(TextField)(({}) => ({
   '& .MuiInputBase-root': {
     padding: '7px',
     height: '40px',
-    width: fullWidth ? '100%' : 'auto'
+    width: '100%',
   },
   '& input::placeholder': {}
-}))
-const CustomTextField: React.FC<CustomTextFieldProps> = ({ label, name, placeholder, fullWidth, onChange, ...rest }) => {
+}));
+
+const CustomTextField = ({ label, name, control, placeholder, helperMessage, field, ...rest } ) => {
+  const renderField = field ? field : (
+    <Controller
+      name={name}
+      control={control}
+      rules={{
+        required: true,
+      }}
+      render={({ field, fieldState }) => (
+        <StyledTextField
+          {...field}
+          placeholder={label}
+          error={fieldState.invalid}
+          helperText={fieldState.invalid ? helperMessage : ''}
+          {...rest}
+        />
+      )}
+    />
+  );
+
   return (
     <>
       <StyledLabel htmlFor={name}>{label}</StyledLabel>
       <br />
-      <StyledTextField
-        name={name}
-        placeholder={placeholder || label}
-        fullWidth={fullWidth}
-        onChange={onChange}
-        {...rest}
-      />
+      {renderField}
     </>
-  )
-}
-export default CustomTextField
+  );
+};
+
+export default CustomTextField;
