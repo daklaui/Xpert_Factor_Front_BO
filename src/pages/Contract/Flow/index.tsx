9aa574444905ed6ad171f76495a5551c69631c13
Anis Allagui <annousanisallag123@gmail.com>
2024-02-19 12:07:04 +0100

    styled text field fake modification

--- src/pages/Contract/Flow/index.tsx
+++ src/pages/Contract/Flow/index.tsx
@@ -1,36 +1,13 @@
+
 import React from 'react'
-import { useForm, Controller } from 'react-hook-form'
-import CustomTextField from 'src/@core/components/mui/text-field'
+import GeneralInfo from './GeneralInfo'
 
-function Index() {
-  const { control, handleSubmit } = useForm()
-
-  const onSubmit = data => {
-    console.log(data)
-  }
+function index() {
+  const popperPlacement = 'bottom';
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        name='fullName'
-        control={control}
-        rules={{ required: true, minLength: 7 }}
-        render={({ field, fieldState }) => (
-          <CustomTextField
-            placeholder='fullname'
-            label='Full Name'
-            control={control}
-            {...field}
-            error={fieldState.invalid}
-            helperMessage='aaa'
-          />
-        )}
-      />
-
-      {/* Ajoutez d'autres champs de formulaire ici si nécessaire */}
-      <button type='submit'>Submit</button>
-    </form>
+      <GeneralInfo popperPlacement={popperPlacement} />
   )
 }
 
-export default Index
+export default index
