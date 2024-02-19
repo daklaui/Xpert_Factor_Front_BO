73381723958c392515ac1f997d334d01b63ab885
Nour Lahbibi <nourlahbibi3@gmail.com>
2024-02-19 13:10:54 +0000

    form individu

--- src/pages/individual/Flow/index.tsx
+++ src/pages/individual/Flow/index.tsx
@@ -1,5 +1,4 @@
-import FormIndividual from "src/SharedComponents/FormIndividual"
-
+import FormIndividual from "C:/Xpert_Factor_Front_BO/src/pages/individual/Flow/FormIndividual"
 const IndividualFlow = () => {
   return <FormIndividual/>
 }
