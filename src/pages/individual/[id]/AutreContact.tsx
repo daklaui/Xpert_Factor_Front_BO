485ccb31e58fc978be60cddfdd822eab918eb945
eyaboulaabi <eyaboulaabi1@gmail.com>
2024-02-20 00:13:18 +0100

    details individu with lazy loading

---
+++ src/pages/individual/[id]/AutreContact.tsx
@@ -0,0 +1,35 @@
+import TabPanel from '@mui/lab/TabPanel'
+import {
+  CardContent,
+  Checkbox,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from '@mui/material'
+import { Suspense, lazy } from 'react'
+import TableContact from './tables/TableContact'
+
+const Tabcontact = lazy(() => import('./tables/TableContact'))
+
+const AutreContact = () => {
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <CardContent>
+        <TabPanel sx={{ p: 0 }} value='autre-contacts'>
+          <Grid container spacing={5}>
+            <Grid item xs={12}>
+              <Suspense fallback={<div>Loading...</div>}>
+                <TableContact />
+              </Suspense>
+            </Grid>
+          </Grid>
+        </TabPanel>
+      </CardContent>
+    </form>
+  )
+}
+export default AutreContact
--- src/pages/individual/[id]/index.tsx
+++ src/pages/individual/[id]/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { Button, Card, CardHeader, CircularProgress, Grid, Tab, Typography } from '@mui/material'
@@ -18,8 +19,84 @@ const LazyListeRib = lazy(() => import('./Tables/TableRib'))
 
 const Index = () => {
   const router = useRouter()
+  const [value, setValue] = useState('info-individu')
 
-  return router.query.id
+  const { id } = router.query
+  const individuId = id
+
+  useEffect(() => {
+    setValue('info-individu')
+  }, [individuId])
+
+  const handleTabsChange = (event: React.ChangeEvent<any>, newValue: string) => {
+    setValue(newValue)
+  }
+
+  return (
+    <>
+      <Grid container alignItems='center' justifyContent='space-between'>
+        <Typography variant='h2' component='div' sx={{ mb: 2 }}>
+          Individu
+        </Typography>
+        <Grid item>
+          <Button variant='contained'>Modifier Individu</Button>
+          <Button variant='contained' sx={{ marginLeft: '8px' }}>
+            Liste des Individus
+          </Button>
+        </Grid>
+      </Grid>
+      <Card>
+        <CardHeader title={<span style={{ fontSize: '20px' }}>Détails d'individu</span>} />
+        <TabContext value={value}>
+          <TabList
+            variant='scrollable'
+            scrollButtons={false}
+            onChange={handleTabsChange}
+            sx={{
+              borderBottom: theme => `1px solid ${theme.palette.divider}`,
+              '& .MuiTab-root': { py: 1.5, width: '210px' }
+            }}
+          >
+            {[
+              { label: "Info d'individu", icon: <AccountCircleIcon />, value: 'info-individu' },
+              { label: 'Autre informations', icon: <PersonIcon />, value: 'autre-informations' },
+              { label: 'Autre contacts', icon: <ContactPhoneIcon />, value: 'autre-contacts' },
+              { label: 'Login web', icon: <LoginIcon />, value: 'login-web' },
+              { label: 'Liste RIB', icon: <FormatListNumberedIcon />, value: 'liste-RIB' }
+            ].map(tab => (
+              <Tab
+                key={tab.value}
+                value={tab.value}
+                label={
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    {tab.icon} {tab.label}
+                  </div>
+                }
+              />
+            ))}
+          </TabList>
+          <Suspense fallback={<CircularProgress />}>
+            {value === 'info-individu' && <LazyInfoIndividu />}
+            {value === 'autre-informations' && <LazyAutreInfo />}
+            {value === 'autre-contacts' && <LazyAutreContact />}
+            {value === 'login-web' && <LazyLoginWeb />}
+            {value === 'liste-RIB' && <LazyListeRib />}
+          </Suspense>
+        </TabContext>
+      </Card>
+    </>
+  )
 }
 
+export default Index
+=======
+import React from 'react'
+import { useRouter } from 'next/router'
+import DetailsIndividu from './DetailsIndividu'
+const ConsulationIndividual = () => {
+  const router = useRouter()
+  const { id } = router.query
+  return <DetailsIndividu individuId={id} />
+}
 export default ConsulationIndividual
+>>>>>>> 5d2ea3a (details individu with lazy loading)
