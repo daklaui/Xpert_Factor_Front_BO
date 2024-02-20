0f3366916db605c46cf6d3b01384a5a3743831b4
Dakhloaui moataz <moatez@shamash-it.com>
2024-02-20 21:06:16 +0100

    .

--- src/pages/individual/Flow/Form_Individual/Steps/Step0Company.tsx
+++ src/pages/individual/Flow/Form_Individual/Steps/Step0Company.tsx
@@ -1,10 +1,11 @@
-import { useState, Fragment, useEffect } from "react";
-import DatePicker from 'react-datepicker';
-import Select from 'react-select';
-import 'react-datepicker/dist/react-datepicker.css';
-import { Grid, TextField, RadioGroup, FormControlLabel, Radio, Box } from "@mui/material";
-import { getJuridique, getActivite, getGroupeSociete } from '../mock';
-import { CustomSelectProps , SelectInerface } from '../interface/FormIndivInterfaces';
+import { useState, Fragment, useEffect } from 'react'
+import DatePicker from 'react-datepicker'
+import Select from 'react-select'
+import 'react-datepicker/dist/react-datepicker.css'
+import { Grid, TextField, RadioGroup, FormControlLabel, Radio, Box } from '@mui/material'
+import { getJuridique, getActivite, getGroupeSociete } from '../mock'
+import { CustomSelectProps, SelectInerface } from '../interface/FormIndividual.interface'
+
 const Step0Company = ({ onSearch, labelText }: CustomSelectProps) => {
   const [optionsJuridique, setOptionsJuridique] = useState<SelectInerface[]>([])
   const [statusValueJuridique, setStatusValueJuridique] = useState('')
@@ -29,19 +30,19 @@ const Step0Company = ({ onSearch, labelText }: CustomSelectProps) => {
     lieu: '',
     grp_soc: '',
     activite: '',
-    selectDisabled: false,
-  };
-  
-  const [companyData, setCompanyData] = useState(initialCompanyState);
+    selectDisabled: false
+  }
+
+  const [companyData, setCompanyData] = useState(initialCompanyState)
 
   function handleChange(field: string, value: string): void {
-    let updatedData = { ...companyData, [field]: value };
+    let updatedData = { ...companyData, [field]: value }
 
     if (field === 'grp_soc') {
-      updatedData = { ...updatedData, selectDisabled: value === 'Non' };
+      updatedData = { ...updatedData, selectDisabled: value === 'Non' }
     }
 
-    setCompanyData(updatedData);
+    setCompanyData(updatedData)
   }
 
   return (
@@ -61,15 +62,19 @@ const Step0Company = ({ onSearch, labelText }: CustomSelectProps) => {
         <Grid item xs={12} sm={6}>
           {/* Change all the TextField with anis input */}
           <p>Dénomination </p>
-          <TextField fullWidth value={companyData.denomination} onChange={(e) => handleChange('denomination', e.target.value)} />
+          <TextField
+            fullWidth
+            value={companyData.denomination}
+            onChange={e => handleChange('denomination', e.target.value)}
+          />
         </Grid>
         <Grid item xs={12} sm={6}>
           <p>Nom de commerce </p>
-          <TextField fullWidth value={companyData.nom_com} onChange={(e) => handleChange('nom_com', e.target.value)} />
+          <TextField fullWidth value={companyData.nom_com} onChange={e => handleChange('nom_com', e.target.value)} />
         </Grid>
         <Grid item xs={12} sm={6}>
           <p>Register de commerce</p>
-          <TextField fullWidth value={companyData.reg_com} onChange={(e) => handleChange('reg_com', e.target.value)} />
+          <TextField fullWidth value={companyData.reg_com} onChange={e => handleChange('reg_com', e.target.value)} />
         </Grid>
         <Grid item xs={12} sm={6}>
           <p>Activité</p>
@@ -81,24 +86,29 @@ const Step0Company = ({ onSearch, labelText }: CustomSelectProps) => {
         </Grid>
         <Grid item xs={12} sm={6}>
           <p>Lieu</p>
-          <TextField fullWidth value={companyData.lieu} onChange={(e) => handleChange('lieu', e.target.value)} />
+          <TextField fullWidth value={companyData.lieu} onChange={e => handleChange('lieu', e.target.value)} />
         </Grid>
         <Grid container alignItems='center' item xs={12} sm={6}>
           <p>Groupe de sociétés</p>
-          <RadioGroup row name='groupe-de-societes' onChange={(event) => handleChange('grp_soc', event.target.value)} value={companyData.grp_soc}>
+          <RadioGroup
+            row
+            name='groupe-de-societes'
+            onChange={event => handleChange('grp_soc', event.target.value)}
+            value={companyData.grp_soc}
+          >
             <FormControlLabel value='Oui' control={<Radio />} label='Oui' />
             <FormControlLabel value='Non' control={<Radio />} label='Non' />
           </RadioGroup>
         </Grid>
-            <Grid item xs={12} sm={6}>
-            <p>Groupe de sociétés</p>
-            <Select
-              options={getGroupeSociete}
-              value={{ value: companyData.grp_soc, label: companyData.grp_soc }}
-              onChange={(getGroupeSociete: { value: string; }) => handleChange('grp_soc', getGroupeSociete.value)}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={6}>
+          <p>Groupe de sociétés</p>
+          <Select
+            options={getGroupeSociete}
+            value={{ value: companyData.grp_soc, label: companyData.grp_soc }}
+            onChange={(getGroupeSociete: { value: string }) => handleChange('grp_soc', getGroupeSociete.value)}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
           <p>Date de création</p>
           <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
             <div>
@@ -110,8 +120,7 @@ const Step0Company = ({ onSearch, labelText }: CustomSelectProps) => {
           </Box>
         </Grid>
       </Fragment>
-    
     </>
   )
 }
-export default Step0Company 
+export default Step0Company
