281161edee8135959d8859405d4d6133d907e2f4
Nour Lahbibi <nourlahbibi3@gmail.com>
2024-02-20 20:52:41 +0000

    formindiv

---
+++ src/pages/individual/Flow/Form_Individual/PickersCustomInput.tsx
@@ -0,0 +1,19 @@
+// ** React Imports
+import { forwardRef } from 'react'
+
+// ** MUI Imports
+import TextField from '@mui/material/TextField'
+
+import {PickerProps} from './interface/FormIndivInterfaces'
+
+
+const PickersComponent = forwardRef(({ ...props }: PickerProps, ref) => {
+  // ** Props
+  const { label, readOnly } = props
+
+  return (
+    <TextField inputRef={ref} {...props} label={label || ''} {...(readOnly && { inputProps: { readOnly: true } })} />
+  )
+})
+
+export default PickersComponent
--- src/pages/individual/Flow/index.tsx
+++ src/pages/individual/Flow/index.tsx
@@ -1,6 +1,5 @@
-import FormIndividual from 'C:/Xpert_Factor_Front_BO/src/pages/individual/Flow/FormIndividual'
+import FormIndividual from '../Flow/FormIndividual'
 
-// fixe this
 const IndividualFlow = () => {
   return <FormIndividual />
 }
