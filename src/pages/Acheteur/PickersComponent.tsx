6c38ca155bee677005e9be5952860bb463af87a2
Anis Allagui <annousanisallag123@gmail.com>
2024-02-21 21:45:54 +0100

    AddContract anis_allagui

---
+++ src/pages/Acheteur/PickersComponent.tsx
@@ -0,0 +1,17 @@
+
+import { forwardRef } from 'react'
+import TextField from '@mui/material/TextField'
+
+interface PickerProps {
+  label?: string
+  readOnly?: boolean
+}
+const PickersComponent = forwardRef(({ ...props }: PickerProps, ref) => {
+
+  const { label, readOnly } = props
+ 
+  return (
+    <TextField inputRef={ref} {...props} label={label || ''} {...(readOnly && { inputProps: { readOnly: true } })} />
+  )
+})
+export default PickersComponent
