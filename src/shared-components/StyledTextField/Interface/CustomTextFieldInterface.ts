e3707c8f05a01b47f054185666d8aa92d4b585fc
Anis Allagui <annousanisallag123@gmail.com>
2024-02-21 21:45:54 +0100

    AddContract anis_allagui

---
+++ src/shared-components/StyledTextField/Interface/CustomTextFieldInterface.ts
@@ -0,0 +1,10 @@
+
+interface CustomTextFieldProps {
+  label: string
+  name: string
+  placeholder?: string
+  fullWidth?: boolean
+  onChange?: (event: any) => void
+}
+
+export default CustomTextFieldProps
--- src/shared-components/StyledTextField/StyledTextField .tsx
+++ src/shared-components/StyledTextField/StyledTextField .tsx
@@ -1,29 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react'
 import TextField from '@mui/material/TextField'
 import { styled } from '@mui/material/styles'
+import CustomTextFieldProps from './Interface/CustomTextFieldInterface'
+import StyledLabel from 'src/pages/Contract/Flow/StyledInputs/StyledLabel'
 
-const StyledLabel = styled('label')({
-  fontWeight: 'bold',
-  fontSize: '13px',
-  paddingBottom: '12px',
-  marginBottom: '110px'
-})
-
-const StyledTextField = styled(TextField)(({}) => ({
+const StyledTextField : any = styled(TextField)(({ fullWidth }: CustomTextFieldProps) => ({
   '& .MuiInputBase-root': {
     padding: '7px',
     height: '40px',
-    width: '100%'
+    width: fullWidth ? '100%' : 'auto'
   },
   '& input::placeholder': {}
 }))
 
-const CustomTextField = ({ label, name, placeholder, ...rest }) => {
+const CustomTextField: React.FC<CustomTextFieldProps> = ({ label, name, placeholder, fullWidth, onChange, ...rest }) => {
   return (
     <>
       <StyledLabel htmlFor={name}>{label}</StyledLabel>
       <br />
-      <StyledTextField name={name} placeholder={label} {...rest} />
+      <StyledTextField
+        name={name}
+        placeholder={placeholder || label}
+        fullWidth={fullWidth}
+        onChange={onChange}
+        {...rest}
+      />
     </>
   )
 }
