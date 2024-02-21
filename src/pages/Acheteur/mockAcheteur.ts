7143ad6e0de893df263a438ff9f79ceb1c9b9078
eyaboulaabi <eyaboulaabi1@gmail.com>
2024-02-21 17:00:13 +0100

    Ajouter Acheteur

---
+++ src/pages/Acheteur/mockAcheteur.ts
@@ -0,0 +1,40 @@
+// mock data for adherents
+export const getAdherentsData = () => {
+  return {
+    AD1: [
+      { id: 1, nom: 'Nom1', type: 'Type1', numero: 'Numero1' },
+      { id: 2, nom: 'Nom2', type: 'Type2', numero: 'Numero2' },
+    ],
+    AD2: [
+      { id: 3, nom: 'Nom3', type: 'Type3', numero: 'Numero3' },
+      { id: 4, nom: 'Nom4', type: 'Type4', numero: 'Numero4' },
+    ],
+    AD3: [
+      { id: 5, nom: 'Nom5', type: 'Type5', numero: 'Numero5' },
+      { id: 6, nom: 'Nom6', type: 'Type6', numero: 'Numero6' },
+    ],
+    AD4: [
+      { id: 7, nom: 'Nom7', type: 'Type7', numero: 'Numero7' },
+      { id: 8, nom: 'Nom8', type: 'Type8', numero: 'Numero8' },
+    ],
+    AD5: [
+      { id: 9, nom: 'Nom9', type: 'Type9', numero: 'Numero9' },
+      { id: 10, nom: 'Nom10', type: 'Type10', numero: 'Numero10' },
+    ],
+    AD6: [
+      { id: 11, nom: 'Nom11', type: 'Type11', numero: 'Numero11' },
+      { id: 12, nom: 'Nom12', type: 'Type12', numero: 'Numero12' },
+    ],
+    AD7: [
+      { id: 13, nom: 'Nom13', type: 'Type13', numero: 'Numero13' },
+      { id: 14, nom: 'Nom14', type: 'Type14', numero: 'Numero14' },
+    ],
+    AD8: [
+      { id: 15, nom: 'Nom15', type: 'Type15', numero: 'Numero15' },
+      { id: 16, nom: 'Nom16', type: 'Type16', numero: 'Numero16' },
+    ],
+  };
+};
+
+
+  
